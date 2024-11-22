0fbc1bc70ef164ab614c88551bf6877d8488971b
Ahmad Raihan <ahmad.reyhan2230@gmail.com>
2024-11-22 14:46:41 +0700

    add login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule , } from '@angular/forms';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { PasswordModule } from 'primeng/password';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FloatLabelModule, CommonModule, FormsModule, PasswordModule], 
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']  
+})
+
+
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  // Fungsi login
+  onLogin() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: () => {
+        this.errorMessage = 'Login gagal. Periksa username dan password.';
+      }
+    });
+  }
+}
